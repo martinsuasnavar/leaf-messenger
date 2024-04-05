81b06340b8997dc746402734c5d3865922730aea
Martín Suasnavar <msuasnavar21@gmail.com>
2024-04-05 15:43:10 -0300

    Update web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -33,3 +33,34 @@ Route::get('/check-database-connection', function () {
 });
 
 
+
+////index
+//->sessions
+//->rooms
+//->users
+//->messages
+
+//SESSIONS////////////////
+Route::get('/api/sessions', [SessionController::class, 'getAll']);
+Route::post('/api/create-session', [SessionController::class, 'create']);
+Route::delete('/api/delete-session/{session_id}', [SessionController::class, 'delete']);
+////////////////
+
+//ROOMS/////////////////
+Route::get('/api/rooms', [RoomController::class, 'getAll']);
+Route::post('/capi/reate-room', [RoomController::class, 'create']);
+Route::get('/api/room/{room_id}', [RoomController::class, 'getById']);
+////////////////
+
+//USERS////////////////
+Route::get('/api/users', [UserController::class, 'getAll']);
+Route::post('/api/create-user', [UserController::class, 'create']);
+////////////////
+
+
+
+//MESSAGES////////////////
+Route::get('/messages', [MessageController::class, 'getAll']);
+Route::post('/create-message', [MessageController::class, 'create']);
+////////////////
+
